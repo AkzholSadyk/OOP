ad8ecd3c87b49c28f3e638e4b3a1c5fb83f68dc3
aksa001 <akzholsadyk12@gmail.com>
2024-11-01 19:20:02 +0600

    lab05

--- src/Pracitce05/Example2.java
+++ src/Pracitce05/Example2.java
@@ -5,16 +5,15 @@ public class Example2 {
         int i, ratio;
         int[] numbers = {100, 10, 0, 5, 2, 8, 0, 30};
 
-        for (i = 0; i < numbers.length - 1; i++) {
-            try {
-                if (numbers[i + 1] == 0) {
-                    throw new ArithmeticException();
-                }
-                ratio = numbers[i] / numbers[i + 1];
-                System.out.println(numbers[i] + "/" + numbers[i + 1] + "=" + ratio);
-            } catch (ArithmeticException ae) {
-                System.out.println("Couldn't calculate " + numbers[i] + "/" + numbers[i + 1]);
+
+        for (i=0; i < numbers.length-1; i++){
+            try{
+                ratio = numbers[i] / numbers[i+1];
+                System.out.println(numbers[i]+"/"+numbers[i+1]+"="+ratio);
+            }catch(ArithmeticException a) {
+                System.out.println("Couldn't calculate "+numbers[i]+"/"+numbers[i+1]);
             }
+
         }
     }
 }
--- src/Pracitce05/Example3.java
+++ src/Pracitce05/Example3.java
@@ -7,16 +7,20 @@ public class Example3 {
         int[]            data = {50, 320, 97, 12, 2000};
 
 
-        try
-        {
-            for (i=0; i < 10; i++)
-            {
-                System.out.println(data[i]);
-            }
-        }
-        catch (ArrayIndexOutOfBoundsException aioobe)
-        {
-            System.out.println("Done");
+//        try
+//        {
+//            for (i=0; i < 10; i++)
+//            {
+//                System.out.println(data[i]);
+//            }
+//        }
+//        catch (ArrayIndexOutOfBoundsException aioobe)
+//        {
+//            System.out.println("Done");
+//        }
+
+        for(i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
         }
     }
 }
