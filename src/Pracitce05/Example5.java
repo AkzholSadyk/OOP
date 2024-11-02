145bd555db330b07e869ed48946bc396972b45b4
aksa001 <akzholsadyk12@gmail.com>
2024-11-02 13:24:03 +0600

    lab05

--- src/Pracitce05/Example5.java
+++ src/Pracitce05/Example5.java
@@ -5,62 +5,13 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+import java.util.StringTokenizer;
 public class Example5 {
-
     public static void main(String[] args) {
-        double leftOperand, result, rightOperand;
-        String leftString, operator, rightString, space;
-        StringTokenizer        tokenizer;
-        Scanner in = new Scanner(System.in);
-        String s1 = in.nextLine();
-        tokenizer = new StringTokenizer(s1, "+-*/ ", true);
-
-        try{
-            while (tokenizer.hasMoreTokens()) {
-                leftString = tokenizer.nextToken().trim();
-
-                if (leftString.equals("")) {
-                    continue;
-                }
-
-                operator = tokenizer.nextToken().trim();
-                if (operator.equals("")) {
-                    operator = tokenizer.nextToken().trim();
-                }
-
-                rightString = tokenizer.nextToken().trim();
-                if (rightString.equals("")) {
-                    rightString = tokenizer.nextToken().trim();
-                }
-
-                leftOperand = Double.parseDouble(leftString);
-                rightOperand = Double.parseDouble(rightString);
-
-                if (operator.equals("+")) {
-                    result = leftOperand + rightOperand;
-                } else if (operator.equals("-")) {
-                    result = leftOperand - rightOperand;
-                } else if (operator.equals("*")) {
-                    result = leftOperand * rightOperand;
-                } else if (operator.equals("/")) {
-                    result = leftOperand / rightOperand;
-                } else {
-                    System.out.println("Unknown operator: " + operator);
-                    continue;
-                }
-
-                System.out.println("Result: " + result);
-            }
+        String a="KBTU+SDU,IT";
+        StringTokenizer st=new StringTokenizer(a,",+",true);
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
         }
-        catch (NoSuchElementException nsee)
-        {
-            System.out.println("Invalid syntax");
-        }
-        catch (NumberFormatException nfe)
-        {
-            System.out.println("One or more operands is not a number");
-        }
-
     }
-
 }
